d0473633e91b62e3317a0a1c596fcdb984245dd0
Brad Bird <brad.bird.x@gmail.com>
2016-07-06 20:15:40 +0100

    Setup landing page properly and setup basic portfolio index

---
+++ angular/app/components/portfolio-index/portfolio-index.component.js
@@ -0,0 +1,16 @@
+class PortfolioIndexController {
+    constructor() {
+        'ngInject';
+    }
+
+    $onInit() {
+
+    }
+}
+
+export const PortfolioIndexComponent = {
+    templateUrl: './views/app/components/portfolio-index/portfolio-index.html',
+    controller: PortfolioIndexController,
+    controllerAs: 'vm',
+    bindings: {}
+};
--- angular/index.components.js
+++ angular/index.components.js
@@ -1,12 +1,26 @@
-import {ResetPasswordComponent} from './app/components/reset-password/reset-password.component';
-import {ForgotPasswordComponent} from './app/components/forgot-password/forgot-password.component';
-import {LoginFormComponent} from './app/components/login-form/login-form.component';
-import {RegisterFormComponent} from './app/components/register-form/register-form.component';
-import {SocialIconsComponent} from './app/components/social-icons/social-icons.component';
+import {
+    ResetPasswordComponent
+} from './app/components/reset-password/reset-password.component';
+import {
+    ForgotPasswordComponent
+} from './app/components/forgot-password/forgot-password.component';
+import {
+    LoginFormComponent
+} from './app/components/login-form/login-form.component';
+import {
+    RegisterFormComponent
+} from './app/components/register-form/register-form.component';
+import {
+    SocialIconsComponent
+} from './app/components/social-icons/social-icons.component';
+import {
+    PortfolioIndexComponent
+} from './app/components/portfolio-index/portfolio-index.component';
 
 angular.module('app.components')
-	.component('socialIcons', SocialIconsComponent)
-	.component('resetPassword', ResetPasswordComponent)
-	.component('forgotPassword', ForgotPasswordComponent)
-	.component('loginForm', LoginFormComponent)
-	.component('registerForm', RegisterFormComponent);
+    .component('socialIcons', SocialIconsComponent)
+    .component('portfolioIndex', PortfolioIndexComponent)
+    .component('resetPassword', ResetPasswordComponent)
+    .component('forgotPassword', ForgotPasswordComponent)
+    .component('loginForm', LoginFormComponent)
+    .component('registerForm', RegisterFormComponent);
